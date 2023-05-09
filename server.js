81492f053670e1755744672e057f537715100bc2
Jijie Zhou <jijiez@email.unc.edu>
2023-05-09 18:19:00 -0400

    a04

---
+++ server.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node 
+
+import minimist from 'minimist';
+import express from "express"
+
+import { rps,rpsls } from "./lib/rpsls.js";
+
+
+const args = minimist(process.argv.slice(2));
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const port = args["port"]  || 5000;
