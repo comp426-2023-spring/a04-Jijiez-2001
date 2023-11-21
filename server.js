6bbde19a7d1790c24ac6c60b5ab1ad81ffe27809
jijiezhou <jijiez@email.unc.edu>
2023-11-21 11:33:24 -0500

    server.js listen ports

--- server.js
+++ server.js
@@ -11,25 +11,72 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/**
+/** get
  * /app/ -> 200 OK
  */
 app.get("/app/", (req, res) => res.status(200).send("200 OK"));
 
-/**
+/** get
  * /app/rps/
  */
 app.get("/app/rps/", (req, res) =>
-  res.json({ player: "(rock|paper|scissors)" })
+  res.status(200).send(JSON.stringify(rps(req.body.shot)))
 );
 
-/**
+/** get
  * /app/rpsls
  */
 app.get("/app/rpsls/", (req, res) =>
-  res.json({ player: "(rock|paper|scissors|lizard|spock)" })
+  res.status(200).send(JSON.stringify(rpsls(req.body.shot)))
 );
 
-/**
- *
+/** get
+ * /app/rps/play
  */
+app.get("/app/rps/play", (req, res) =>
+  res.status(200).send(JSON.stringify(rps(req.query.shot)))
+);
+
+/** get
+ * /app/rpsls/play
+ */
+app.get("/app/rpsls/play", (req, res) =>
+  res.status(200).send(JSON.stringify(rpsls(req.query.shot)))
+);
+
+/** post
+ * /app/rps/play
+ */
+app.post("/app/rps/play", (req, res) =>
+  res.status(200).send(JSON.stringify(rps(req.body.shot)))
+);
+
+/** post
+ * /app/rpsls/play
+ */
+app.post("/app/rpsls/play", (req, res) =>
+  res.status(200).send(JSON.stringify(rpsls(req.body.shot)))
+);
+
+/** get
+ * /app/rps/play/:shot
+ */
+app.get("/app/rps/play/:shot", (req, res) =>
+  res.status(200).send(JSON.stringify(rps(req.params.shot)))
+);
+
+/** get
+ * /app/rpsls/play/:shot
+ */
+app.get("/app/rpsls/play/:shot", (req, res) =>
+  res.status(200).send(JSON.stringify(rpsls(req.params.shot)))
+);
+
+/** get
+ * default
+ */
+app.get("*", (req, res) => {
+  res.status(404).send("404 NOT FOUND");
+});
+
+app.listen(port);
